8c31d8a356c625225c57d8e86cce2046200eea2b
Matthew Peroni <matthewperoni@matthews-mbp.myfiosgateway.com>
2021-05-12 23:22:24 -0400

    update index to IP

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
 from functools import lru_cache
+from sklearn.preprocessing import normalize
 import faiss
 import numpy
 import json
@@ -7,7 +8,7 @@ import numpy as np
 
 
 model = SentenceTransformer('stsb-distilbert-base')
-index = faiss.IndexFlatL2(768)
+index = faiss.IndexFlatIP(768)
 precomputed_movie_embs = np.load('data/model/movie_embeddings.npy')
 index.add(precomputed_movie_embs)
 f = open('data/model/embedding_idx_to_movie_id.json')
@@ -16,17 +17,10 @@ embedding_idx_to_id = {
     int(key): value for key, value in embedding_idx_to_id.items()
 }
 
-
-def normalize(v):
-    norm = np.linalg.norm(v)
-    if norm == 0: 
-        return v
-    return v / norm
-
 @lru_cache(maxsize=None)
 def get_recommended_movies(searchText, numToReturn):
     search_embedding = model.encode(searchText)
-    search_embedding = np.array([normalize(search_embedding)]).reshape((1, 768))
+    search_embedding = normalize(search_embedding.reshape(1,-1), norm='l2')
     if numToReturn > 250:
         numToReturn = 250
     _, I = index.search(search_embedding, numToReturn)
