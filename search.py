f11af8980dcfb3deda66dd878d67761f80e2a7ac
Matthew Peroni <matthewperoni@dhcp-vl2043-2258.cornell-visitor.cit.cornell.edu>
2021-05-22 15:14:45 -0400

    updated notebooks, simplified search fn

--- search.py
+++ search.py
@@ -3,7 +3,6 @@ from functools import lru_cache
 from sklearn.preprocessing import normalize
 import faiss
 import numpy
-import json
 import numpy as np
 
 
@@ -11,11 +10,7 @@ model = SentenceTransformer('stsb-distilbert-base')
 index = faiss.IndexFlatIP(768)
 precomputed_movie_embs = np.load('data/model/movie_embeddings.npy')
 index.add(precomputed_movie_embs)
-f = open('data/model/embedding_idx_to_movie_id.json')
-embedding_idx_to_id = json.load(f)
-embedding_idx_to_id = {
-    int(key): value for key, value in embedding_idx_to_id.items()
-}
+embedding_idx_to_id = np.load('data/model/embedding_idx_to_movie_id.npy')
 
 @lru_cache(maxsize=None)
 def get_recommended_movies(searchText, numToReturn):
